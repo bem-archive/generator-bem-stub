d5850d4d26c447479d1793aee16a93cfb403fc54
Evgeniy Gavrushin <job.govorov@yandex.ru>
2014-03-26 18:30:21 +0400

    'tools' with 'css' as 'preprocessor'

---
+++ test/basic/tools.bemcore.addLibs.platforms.techs.7/make.js
@@ -0,0 +1,52 @@
+/* jshint node:true */
+/* global MAKE */
+
+var environ = require('bem-environ')(__dirname);
+environ.extendMake(MAKE);
+
+//process.env.YENV = 'production';
+//process.env.XJST_ASYNCIFY = 'yes';
+
+MAKE.decl('Arch', {
+
+    blocksLevelsRegexp: /^.+?\.blocks/,
+    bundlesLevelsRegexp: /^.+?\.bundles$/,
+
+    libraries: [
+        'bem-core @ v2.0.0',
+        'bem-components @ v2',
+        'bem-mvc @ v2'
+    ]
+
+});
+
+
+MAKE.decl('BundleNode', {
+
+    getTechs: function() {
+
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'css',
+            'ie.css',
+            'ie6.css',
+            'ie7.css',
+            'ie8.css',
+            'ie9.css',
+            'bemtree',
+            'bemhtml',
+            'node.js',
+            'browser.js+bemhtml'
+        ];
+
+    },
+
+    'create-browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
+        sourceNode.getFiles().forEach(function(f) {
+            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
+        }, this);
+    }
+
+});
