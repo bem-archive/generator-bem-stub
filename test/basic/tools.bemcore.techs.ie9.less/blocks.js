2c5038471fedc560d2deccdea1544af37e3785c7
Evgeniy Gavrushin <job.govorov@yandex.ru>
2014-03-29 13:24:26 +0200

    Base tests for 'bem-tools'

---
+++ test/basic/tools.bemcore.techs.ie9.less/blocks.js
@@ -0,0 +1,23 @@
+var PATH = require('path'),
+    environ = require('bem-environ'),
+    join = PATH.join,
+
+    PRJ_ROOT = environ.PRJ_ROOT,
+    PRJ_TECHS = join(PRJ_ROOT, '.bem/techs'),
+    BEMCORE_TECHS = environ.getLibPath('bem-core', '.bem/techs');
+    BEMBL_TECHS = environ.getLibPath('bem-bl', 'blocks-common/i-bem/bem/techs/v2');
+
+exports.getTechs = function() {
+
+    return {
+        'bemdecl.js'           : 'v2/bemdecl.js',
+        'deps.js'              : 'v2/deps.js',
+        'css'                  : 'v2/css',
+        'ie.css'               : 'v2/ie.css',
+        'ie9.css'              : 'v2/ie9.css',
+        'less'                 : 'v2/less'
+    };
+
+};
+
+exports.defaultTechs = ['css', 'browser.js', 'bemhtml'];
