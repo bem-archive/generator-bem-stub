4e47635bed1236b8450f11cd7df3b451811067cf
Evgeniy Gavrushin <job.govorov@yandex.ru>
2014-04-09 21:46:00 +0300

    Updated config

--- app/templates/enb/.enb/make.js
+++ app/templates/enb/.enb/make.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
         ]);
     });
 
-
+    // Minimization
     config.mode('development', function(modeConfig) {
         config.nodes('*.bundles/*', function(nodeConfig) {
             nodeConfig.addTechs([
