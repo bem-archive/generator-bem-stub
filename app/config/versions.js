b133a32761abf3ed481eab38c575f931b9e8e7f1
Evgeniy Gavryushin <job.egavr@yandex.ru>
2014-12-02 15:37:00 +0300

    Update versions of libs and deps

--- app/config/versions.js
+++ app/config/versions.js
@@ -1,19 +1,19 @@
 module.exports = {
     libs: {
         'bem-core': 'v2.5.0',
-        'bem-components': '451bec8d7277ff9a39e6eecfefa1ae78fdaf6892'
+        'bem-components': '7329bcb78aaee74ab3372d93a026205d7c0ae77e'
     },
     deps: {
         // enb
-        enb: '0.13.7',
-        'enb-autoprefixer': '0.1.1',
-        'enb-bemxjst': '1.3.2',
-        'enb-bem-techs': '1.0.0-rc',
-        'enb-bh': '0.2.2',
-        'enb-borschik': '1.1.1',
+        enb: '0.13.9',
+        'enb-autoprefixer': '0.1.2',
+        'enb-bemxjst': '1.3.3',
+        'enb-bem-techs': '1.0.0-rc2',
+        'enb-bh': '0.4.0',
+        'enb-borschik': '1.4.0',
         'enb-diverse-js': '0.1.0',
         'enb-modules': '0.2.0',
-        'enb-stylus': '1.1.4',
+        'enb-stylus': '1.1.5',
 
         // bem
         bem: '0.9.0',
@@ -21,11 +21,11 @@ module.exports = {
         'bem-tools-autoprefixer': '0.0.3',
 
         // other
-        bh: '3.0.0',
-        bower: '1.3.9',
-        'borschik-tech-cleancss': '1.0.0',
+        bh: '3.2.2',
+        bower: '1.3.12',
+        'borschik-tech-cleancss': '1.0.3',
         'bower-npm-install': '0.5.9',
-        less: '1.7.4',
-        stylus: '0.47.3'
+        less: '2.1.1',
+        stylus: '0.49.3'
     }
 };
