6d4626ad556781980e0a1d12e4a0953261a0f09d
Vladimir Grinenko <i@tadatuta.com>
2015-07-11 16:36:35 +0300

    Fix autoprefixer config. Closes #131

--- test/unit/unit.js
+++ test/unit/unit.js
@@ -219,7 +219,7 @@ describe('\'ENB\'', function () {
                 'touch-phone': ['common', 'touch', 'touch-phone']
             },
             output = {
-                desktop: ['last 2 versions', 'ie 10', 'ff 24', 'opera 12.16'],
+                desktop: ['last 2 versions', 'ie 10', 'ff 24', 'opera 12.1'],
                 'touch-pad': ['android 4', 'ios 5'],
                 'touch-phone': ['android 4', 'ios 6', 'ie 10']
             };
@@ -442,7 +442,7 @@ describe('\'bem-tools\'', function () {
                 'touch-phone': ['common', 'touch', 'touch-phone']
             },
             output = {
-                desktop: ['last 2 versions', 'ie 10', 'ff 24', 'opera 12.16'],
+                desktop: ['last 2 versions', 'ie 10', 'ff 24', 'opera 12.1'],
                 'touch-pad': ['android 4', 'ios 5'],
                 'touch-phone': ['android 4', 'ios 6', 'ie 10']
             };
